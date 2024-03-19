879c588041edc1bba1a1d5f8541c3aa9c000267f
Emanuel Soares dos Santos <62623835+Soares92200@users.noreply.github.com>
2024-03-18 21:02:26 -0300

    Alteração sql

--- consultorio.sql
+++ consultorio.sql
@@ -5,7 +5,5 @@ CREATE TABLE usuario(
 	idUser INT PRIMARY KEY AUTO_INCREMENT,
 	user VARCHAR (30) UNIQUE NOT NULL,
 	email VARCHAR(50),
-	senha VARCHAR (32) NOT NULL,
-	img VARCHAR(27) default 'img/l.png',
-	pontuacao INT(4) default 0
+	senha VARCHAR (32) NOT NULL
 );
