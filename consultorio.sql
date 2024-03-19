46c8d15620e53499772a0e95a776ac31fd22ec61
Emanuel Soares dos Santos <62623835+Soares92200@users.noreply.github.com>
2024-03-18 21:34:21 -0300

    Alteração sql

--- consultorio.sql
+++ consultorio.sql
@@ -1,9 +1,27 @@
 DROP  DATABASE IF  EXISTS consultorio;
 CREATE DATABASE IF NOT EXISTS  consultorio; 
-USE consultorio;
-CREATE TABLE usuario(
+
+CREATE TABLE consultorio.usuarios(
 	idUser INT PRIMARY KEY AUTO_INCREMENT,
-	user VARCHAR (30) UNIQUE NOT NULL,
-	email VARCHAR(50),
+	user VARCHAR (60),
+	data_de_nascimento VARCHAR(10),
+	genero VARCHAR(1),
+	telefone INT(11),
+	email VARCHAR(50) UNIQUE NOT NULL,
 	senha VARCHAR (32) NOT NULL
+);
+CREATE TABLE consultorio.medicos(
+	idMedico INT PRIMARY KEY AUTO_INCREMENT,
+	medico VARCHAR(60),
+	especialidade VARCHAR(40)
+);
+CREATE TABLE consultorio.agenda(
+	idAgenda INT PRIMARY KEY AUTO_INCREMENT,
+	idUser INT(8) NOT NULL,
+	idMedico INT(8) NOT NULL,
+	data_de_consulta VARCHAR(10) NOT NULL,
+	forma_de_consulta VARCHAR(10) NOT NULL,
+	motivo LONGTEXT,
+	FOREIGN KEY(idUser) REFERENCES usuarios(idUser),
+	FOREIGN KEY(idMedico) REFERENCES medicos(idMedico),
 );
