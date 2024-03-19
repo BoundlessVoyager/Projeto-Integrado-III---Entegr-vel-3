67d4833f39f2ddcd19fbe761ad48a52cb59266fa
Emanuel Soares dos Santos <62623835+Soares92200@users.noreply.github.com>
2024-03-19 11:24:50 -0300

    Arquivo de conexão

---
+++ Controle/Conect.php
@@ -0,0 +1,28 @@
+<?php
+class Conexao{
+    private $conexao;
+    function __construct(){
+        $host = "localhost"; // Onde o SGBD está?
+        $user =  ""; // Quem irá conectar?
+        $pwd = ""; // Senha do usuário
+        $bd = "consultorio"; // Base de dados usada
+        try{
+            $this->setConexao(new PDO("mysql:host={$host};dbname={$bd}",$user,$pwd));
+            $this->getConexao()->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch(PDOException $ex){
+            echo "<p>Erro em PDO: {$ex->getMessage()}</p>";
+        }catch(Exception $ex){
+            echo "<p>Erro geral do sistema: {$ex->getMessage()}</p>";
+        }
+    }
+    function fecharConexao(){
+        $this->conexao = NULL;
+    }
+    function getConexao(){ // Retorna conexão ativa
+       return $this->conexao;
+    }
+    function setConexao($data){ // Define a conexão
+        $this->conexao = $data;
+    }
+}
+?>
